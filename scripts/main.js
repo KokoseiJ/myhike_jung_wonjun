4ea14ea817cf1c1859f8564b88ab9aa313e6a76c
Wonjun Jung <kokoseij@gmail.com>
2024-10-29 11:41:47 -0700

    main: add name lookup from db

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,20 @@
-function getNameFromAuth() {
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
     firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
         if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            userName = user.displayName;
-            document.querySelector("#name-goes-here").innerText = userName
-
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                let userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("name-goes-here").innerText = userName;
+            })
         } else {
-            // No user is signed in.
-            console.log ("No user is logged in");
+            console.log("No user is logged in."); // Log a message when no user is logged in
         }
-    });
+    })
 }
 
 // Function to read the quote of the day from the Firestore "quotes" collection
@@ -20,7 +22,7 @@ function getNameFromAuth() {
 function readQuote(day) {
     db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
         .onSnapshot(dayDoc => {                                                              //arrow notation
-            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            console.log("current document data: " + JSON.stringify(dayDoc.data()));                          //.data() returns data object
             document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
 
             //Here are other ways to access key-value data fields
@@ -33,5 +35,5 @@ function readQuote(day) {
         });
 }
 
-getNameFromAuth(); //run the function
+insertNameFromFirestore();
 readQuote("tuesday");        //calling the function
